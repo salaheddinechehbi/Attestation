fa421f89a411c4f8fdf15c384e745516a5bd723d
Salah Eddine Chehbi <chehbi.se@gmail.com>
2018-10-29 14:42:13 +0000

    List des Regions

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,5 +1,6 @@
 
 import classes.Region;
+import service.RegionService;
 import util.HibernateUtil;
 
 /*
@@ -16,6 +17,20 @@ public class MainTest {
     
     public static void main(String[] args){
         
-       HibernateUtil.getSessionFactory().openSession();
+       //HibernateUtil.getSessionFactory().openSession();
+       
+//       RegionService rs = new RegionService();
+//       rs.create(new Region("Région de Tanger-Tétouan-Al Hoceïma"));
+//       rs.create(new Region("Région de l'Oriental"));
+//       rs.create(new Region("Région de Fès-Meknès"));
+//       rs.create(new Region("Région de Rabat-Salé-Kénitra"));
+//       rs.create(new Region("Région de Béni Mellal-Khénifra"));
+//       rs.create(new Region("Région de Casablanca-Settat"));
+//       rs.create(new Region("Région de Marrakech-Safi"));
+//       rs.create(new Region("Région de Drâa-Tafilalet"));
+//       rs.create(new Region("Région de Souss-Massa"));
+//       rs.create(new Region("Région de Guelmim-Oued Noun"));
+//       rs.create(new Region("Région de Laâyoune-Sakia El Hamra"));
+//       rs.create(new Region("Région de Dakhla-Oued Ed Dahab"));
     }
 }
